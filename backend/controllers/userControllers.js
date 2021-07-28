7aac9ec745ff2f56243ae82065abf5eaa04da1a1
sunny jain <sunny1388.cse18@chitkara.edu.in>
2021-07-28 11:31:39 +0530

    login route

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -26,11 +26,29 @@ const registerUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       pic: user.pic,
     });
-  }
-  else{
-      res.status(400);
-      throw new Error('Error Occured!');
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!");
   }
 });
 
-module.exports = { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Credentials");
+  }
+});
+
+module.exports = { registerUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userControllers');
+const { registerUser, loginUser } = require('../controllers/userControllers');
 
 const userRoutes = express.Router();
 
 userRoutes.route('/').post(registerUser);
+userRoutes.route('/login').post(loginUser)
 
 module.exports = userRoutes;
